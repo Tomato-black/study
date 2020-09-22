ed3db1f370ab6960612543e9990751f0c5fc48d2
zhang.cao <zhang.cao@100credit.com>
2020-09-22 20:52:39 +0800

    自定义锁

--- src/main/java/com/cz/MyLock.java
+++ src/main/java/com/cz/MyLock.java
@@ -16,31 +16,68 @@ public class MyLock implements Lock {
         sync = new MySync();
     }
 
+    @Override
     public void lock() {
-
+        sync.lock();
     }
 
+    @Override
     public void lockInterruptibly() throws InterruptedException {
 
     }
 
+    public boolean tryAcquire(int acquires){
+        return sync.tryAcquire(acquires);
+    }
+
+    @Override
     public boolean tryLock() {
         return false;
     }
 
+    @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
         return false;
     }
 
+    @Override
     public void unlock() {
-
+        sync.unlock();
     }
 
+    @Override
     public Condition newCondition() {
         return null;
     }
 
     static final class MySync extends AbstractQueuedSynchronizer {
+        final void lock() {
+            acquire(1);
+        }
 
+        final void unlock() {
+            setExclusiveOwnerThread(null);
+            acquire(0);
+        }
+
+        @Override
+        protected final boolean tryAcquire(int acquires) {
+            final Thread thread = Thread.currentThread();
+            int state = getState();
+            if (state == 0) {
+                if (!hasQueuedThreads() && compareAndSetState(0, acquires)) {
+                    setExclusiveOwnerThread(thread);
+                    return true;
+                } else if (thread == getExclusiveOwnerThread()) {
+                    state = state + acquires;
+                    if (state < 0) {
+                        new Error("出错了！");
+                    }
+                    setState(state);
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
---
+++ src/main/java/com/cz/MyLockTest.java
@@ -0,0 +1,40 @@
+package com.cz;
+
+/**
+ * @author : zhang.cao
+ * @date : 2020/9/22 19:47
+ */
+public class MyLockTest {
+
+    private static Integer tag = 0;
+
+    public static void main(String[] args) {
+        MyLock myLock = new MyLock();
+
+        Thread thread1 = new Thread(new Thread1(myLock));
+        Thread thread2 = new Thread(new Thread1(myLock));
+        thread1.start();
+        thread2.start();
+    }
+
+
+    static class Thread1 implements Runnable {
+        private MyLock myLock;
+
+        public Thread1(MyLock myLock) {
+            this.myLock = myLock;
+        }
+
+        @Override
+        public void run() {
+            while (!myLock.tryAcquire(1)) {
+            }
+            for (int i = 0; i < 100000; i++) {
+                tag++;
+            }
+            myLock.unlock();
+            System.out.println(tag);
+        }
+    }
+
+}
